6d606f3f53a26370a2ab5348906b8a6384390484
angelicaw133 <angelicawo426@gmail.com>
2024-02-07 10:15:14 -0500

    d

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
@@ -11,8 +10,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+ 
       </nav>
     </div>
   </header>
