02f57bc0367f1e8a96b01ee7171658dd8008ed55
angelicaw133 <angelicawo426@gmail.com>
2024-03-18 01:27:16 -0400

    n

---
+++ vue-project/src/components/IngredientCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h2>{{ ingredient.name }}</h2>
+        <img :src="ingredient.image" alt="">
+        <h3>{{ "$"+ ingredient.price }}</h3>
+        <button>Add to Cart!</button>
+    </div>
+</template>
+
+<script setup>
+import {checkout} from '@/stores/checkout.js'
+const props = defineProps({
+    ingredient: Object
+})
+</script>
+
+<style>
+.container {
+    font-family: "Open Sans", sans-serif;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+ 	flex-wrap: wrap;
+}
+img {
+    display: block;
+    width: 30vw;
+  padding: 1rem;
+}
+button{
+    margin: 1rem;
+}
+</style>
---
+++ vue-project/src/components/ItemCart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container2">
+        <h1>Checkout</h1>
+        <h2>Add an Item to Cart</h2>
+    </div>
+</template>
+
+<script setup>
+import {checkout} from "@/stores/checkout.js"
+const props = defineProps({
+    cart:Array
+})
+</script>
+
+<style>
+
+</style>
