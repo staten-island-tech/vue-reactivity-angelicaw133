2c16fd2a815d00d429392a9057bc4bca05280081
angelicaw133 <angelicawo426@gmail.com>
2024-03-17 20:51:18 -0400

    Revert "e"
    This reverts commit e5b60df4bb3303a956f212cd0a111c750af2a561.

--- vue-project/src/components/DestCard.vue
+++ vue-project/src/components/DestCard.vue
@@ -1,20 +1,13 @@
 <template>
-    <div class="container">
-        <h2>{{ item.name }}</h2>
-        <img :src="item.image" alt="">
-        <h3>{{ "$" + item.price }}</h3>
+    <div>
 
     </div>
 </template>
 
 <script setup>
-const props = defineProps({
-    item : Object
-})
+
 </script>
 
 <style scoped>
-.container {
-    
-}
+
 </style>
